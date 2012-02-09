bc0f912d628f9863a4540672c94de5e6a4fc96a7
Jin Zhou <ferryzhou@gmail.com>
2012-02-08 20:53:23 -0500

    initial rails creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DnRails::Application.config.secret_token = 'c30b23567c2fccb1031af8d2050d387444e0881a8d0526dafa91dc12d49465fd74050a71ff89fc9ea00ce72baa0c2838805400ef5a9801d1d0da1b6c4354743e'
